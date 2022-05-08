30776c2115a4863351621cf84d841b863200c4d1
analivia1104 <analivia110405@gmail.com>
2022-05-08 19:10:53 -0300

    Envio dos botões de Eixos e Pause

--- DesafioAnimação/canvas.js
+++ DesafioAnimação/canvas.js
@@ -2,7 +2,7 @@ var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var quadrado = canvas.getContext('2d');
+var quadrado1 = canvas.getContext('2d');
 var quadrado2 = canvas.getContext('2d');
 
 var x=200;
@@ -10,14 +10,25 @@ var y=200;
 var dx=4;
 var eixoX = 10;
 var eixoY = 10;
+var achar1;
+var achar2;
 /*------------------------------------------------------PROGRAMAÇÃO----EIXO_X-------------------------------------------*/
-function animate_X (){
-    requestAnimationFrame(animate_X);
+function Parar () {
+    
+    quadrado1.clearRect(0,0,innerWidth, innerHeight);
+    quadrado2.clearRect(0,0,innerWidth, innerHeight);
 
-    quadrado.clearRect(0,0,innerWidth,innerHeight); //limpar toda a tela
+}
 
-    quadrado.fillStyle = "#00BFFF" ;
-    quadrado.fillRect(x,250,200,200); // eixo x; eixo y; altura. largura
+BotaoX () // retorno da função
+
+function BotaoX (){
+    requestAnimationFrame(BotaoX);
+
+    quadrado1.clearRect(0,0,innerWidth,innerHeight); //limpar toda a tela
+
+    quadrado1.fillStyle = "#00BFFF" ;
+    quadrado1.fillRect(x,250,200,200); // eixo x; eixo y; altura. largura
 
     if((x+200)>innerWidth){
         eixoX=-eixoX
@@ -26,8 +37,14 @@ function animate_X (){
     else if (x<0) eixoX =-eixoX;
     x=x+eixoX;
 
-/*******/
+}
 
+BotaoY () // retorno da função 
+
+function BotaoY (){
+    requestAnimationFrame(BotaoY);
+
+    quadrado2.clearRect(0,0,innerWidth,innerHeight); //limpar toda a tela
     quadrado2.fillStyle = "#FF4500" ;
     quadrado2.fillRect(250,y,200,200); // eixo x; eixo y; altura. largura
 
@@ -39,4 +56,3 @@ function animate_X (){
     y=y+eixoY;
 
 }
-animate_X()
